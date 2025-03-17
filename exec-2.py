a8edfd73331d6f859ad20c5de8078a12ab08ad9f
caiovinicio <caio.vinicio17012004@gmail.com>
2025-03-17 19:15:47 -0300

    Create exec-2.py

---
+++ exec-2.py
@@ -0,0 +1,4 @@
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+print("caio")
+print("rio de janeiro")
+print("duque de caxias")
