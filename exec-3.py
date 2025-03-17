78396ea9b13be9ea782f24c1e3a03c5ed777c46e
caiovinicio <caio.vinicio17012004@gmail.com>
2025-03-17 20:38:29 -0300

    Update exec-3.py

--- exec-3.py
+++ exec-3.py
@@ -1,2 +1,2 @@
-3. Letra de música - Faça um programa que imprima na tela o nome de uma música que você gosta (proibido letras do Ju
+#3. Letra de música - Faça um programa que imprima na tela o nome de uma música que você gosta (proibido letras do Ju
 print("a nova era - chefin")
