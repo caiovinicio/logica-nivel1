756e793f2be4f595dd98a6735c7b32266cd97d57
caiovinicio <caio.vinicio17012004@gmail.com>
2025-04-07 19:44:25 -0300

    Create create exec-9.py

---
+++ create exec-9.py
@@ -0,0 +1,9 @@
+#escreva um software que some dois numeros
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("infome o primeiro numero")
+numero1 = int(input())
+print('innfome o seguendo numero')
+numero2 = int(input())
+
+print(numero1+numero2)
