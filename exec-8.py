325938b2c4712eaf02099a7cb8f5d4c92e8cf538
caiovinicio <caio.vinicio17012004@gmail.com>
2025-05-19 20:18:18 -0300

    Update and rename exec-8 to exec-8.py

--- exec-8.py
+++ exec-8.py
@@ -1,5 +1,5 @@
 #8. Implemente um programa que na primeira linha faça um print perguntando o nome do usuário e, na linha 2, chame a função input() para capturar o valor do teclado e armazenar na memória d
-o computador.
+#o computadoro
 print("qual e seu nome?")
 nome = input()
 print(nome)
